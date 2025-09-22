1d510cf71f1d8c2fdbb8219eb69111ea1e7dca0c
Prachit Amin <prachitamin12@gmail.com>
2025-09-21 23:53:47 -0400

    Enemy constructor for basic display

---
+++ code/Enemy.cpp
@@ -0,0 +1,12 @@
+#include "Enemy.h"
+
+ECE_Enemy::ECE_Enemy(){};
+
+ECE_Enemy::ECE_Enemy(const sf::Texture &tex)
+    : sf::Sprite(tex)
+{
+    // scale down sprite
+    float wScale = 80 / this->getLocalBounds().width;
+    float hScale = 80 / this->getLocalBounds().height;
+    this->setScale(wScale, hScale);
+}
--- code/Enemy.h
+++ code/Enemy.h
@@ -1,5 +1,10 @@
 #include <SFML/Graphics.hpp>
 
-class ECE_Enemy : public sf::Sprite {
-public:
+class ECE_Enemy : public sf::Sprite
+{
+  public:
+    ECE_Enemy();
+    explicit ECE_Enemy(const sf::Texture &tex);
+    void moveEnemy(float dx, float dy);
+    auto enemyBounds();
 };
